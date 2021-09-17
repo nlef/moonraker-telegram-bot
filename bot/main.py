f59c8133f6ce6c9fe0792cbdf84b9869f2dfba41
nlef <52351624+nlef@users.noreply.github.com>
2021-09-17 13:24:31 +0300

    added global exception handler

--- bot/main.py
+++ bot/main.py
@@ -45,6 +45,17 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = handle_exception
+
 # some global params
 myId = random.randint(300000)
 host = "localhost"
