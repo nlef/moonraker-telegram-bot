c9f15765ea9dcabb6150d16fc2cc12c6bd6132a9
nlef <52351624+nlef@users.noreply.github.com>
2022-07-21 23:29:36 +0300

    added tests for secrets section

--- tests/configuration_test.py
+++ tests/configuration_test.py
@@ -7,6 +7,7 @@ from bot.configuration import ConfigWrapper  # type: ignore
 CONFIG_PATH = "tests/resources/telegram.conf"
 CONFIG_MINIMAL_PATH = "tests/resources/telegram_minimal.conf"
 CONFIG_TEMPLATE_PATH = "scripts/base_install_template"
+CONFIG_WITH_SECRETS_PATH = "tests/resources/telegram_secrets.conf"
 
 
 def test_template_config_has_no_errors():
@@ -22,6 +23,21 @@ def test_minimal_config_has_no_errors():
 
 
 @pytest.fixture
+def config_secrets_helper():
+    conf = configparser.ConfigParser(allow_no_value=True, inline_comment_prefixes=(";", "#"))
+    conf.read(CONFIG_WITH_SECRETS_PATH)
+    return ConfigWrapper(conf)
+
+
+def test_config_with_secrets_has_no_errors(config_secrets_helper):
+    assert config_secrets_helper.configuration_errors == ""
+
+
+def test_config_with_secrets_is_valid(config_secrets_helper):
+    assert config_secrets_helper.secrets.chat_id == 1661233333 and config_secrets_helper.secrets.token == "23423423334:sdfgsdfg-doroasd"
+
+
+@pytest.fixture
 def config_helper():
     conf = configparser.ConfigParser(allow_no_value=True, inline_comment_prefixes=(";", "#"))
     conf.read(CONFIG_PATH)
@@ -30,3 +46,7 @@ def config_helper():
 
 def test_config_has_no_errors(config_helper):
     assert config_helper.configuration_errors == ""
+
+
+def test_config_bot_is_valid(config_helper):
+    assert config_helper.secrets.chat_id == 16612341234 and config_helper.secrets.token == "23423423334:sdfgsdfg-dfgdfgsdfg"
