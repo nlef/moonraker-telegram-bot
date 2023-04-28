41d7ebb455c4407c4bf41f7b132d83d589b487af
Aleksandr Sirotin <pro.un@yandex.ru>
2023-04-28 13:09:43 +0400

    fix blask, pylint

--- bot/main.py
+++ bot/main.py
@@ -995,16 +995,18 @@ def greeting_message(bot: telegram.Bot) -> None:
     klippy.add_bot_announcements_feed()
     check_unfinished_lapses(bot)
 
+
 def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(("192.255.255.255", 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = "127.0.0.1"
+        sock.connect(("192.255.255.255", 1))
+        ip_address = sock.getsockname()[0]
+    except: # pylint: disable=W0702
+        ip_address = "127.0.0.1"
     finally:
-        s.close()
-    return IP
+        sock.close()
+    return ip_address
+
 
 def start_bot(bot_token, socks):
     request_kwargs = {
