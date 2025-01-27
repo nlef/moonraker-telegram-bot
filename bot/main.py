40946d692454a419d66738103d82e38495e96012
Aline Abler <aline.abler@vshn.ch>
2025-01-27 01:58:51 +0100

    [fix] Properly dismiss reply keyboard if it is not to be used
    A Telegram reply keyboard sticks around until it is explicitly
    dismissed. When the reply keyboard is disabled via config, we must send
    a reply keyboard remove event, in case the keyboard was previously
    enabled. Otherwise it will stick around even if disabled.

--- bot/main.py
+++ bot/main.py
@@ -27,7 +27,20 @@ import emoji
 import httpx
 import orjson
 import telegram
-from telegram import BotCommand, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaAudio, InputMediaDocument, InputMediaPhoto, InputMediaVideo, Message, MessageEntity, ReplyKeyboardMarkup, Update
+from telegram import (
+    BotCommand,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaAudio,
+    InputMediaDocument,
+    InputMediaPhoto,
+    InputMediaVideo,
+    Message,
+    MessageEntity,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    Update,
+)
 from telegram.constants import ChatAction, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import Application, CallbackContext, CallbackQueryHandler, CommandHandler, ContextTypes, MessageHandler, filters
@@ -1190,13 +1203,13 @@ async def greeting_message(bot: telegram.Bot) -> None:
             if config_wrap.configuration_errors:
                 mess += await klippy.get_versions_info(bot_only=True) + config_wrap.configuration_errors
 
-        use_keyboard = configWrap.telegram_ui.send_reply_keyboard
+        use_keyboard = config_wrap.telegram_ui.send_reply_keyboard
         await bot.send_message(
             config_wrap.secrets.chat_id,
             text=mess,
             parse_mode=ParseMode.HTML,
             disable_notification=notifier.silent_status,
-            reply_markup=ReplyKeyboardMarkup(create_keyboard(), resize_keyboard=True) if use_keyboard else None,
+            reply_markup=ReplyKeyboardMarkup(create_keyboard(), resize_keyboard=True) if use_keyboard else ReplyKeyboardRemove(),
         )
 
     await bot.set_my_commands(commands=prepare_commands_list(await klippy.get_macros_force(), config_wrap.telegram_ui.include_macros_in_command_list))
