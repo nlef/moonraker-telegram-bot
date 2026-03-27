e86f7bdd687780877c8a75bdcf82e8385ba775b1
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2026-03-27 20:53:41 +0000

    use hostname from Moonraker in greeting message

--- bot/klippy.py
+++ bot/klippy.py
@@ -140,6 +140,7 @@ class Klippy:
         self.paused: bool = False
         self.state: str = ""
         self.state_message: str = ""
+        self.hostname: str = ""
 
         self.printing_duration: float = 0.0
         self.printing_progress: float = 0.0
@@ -388,6 +389,7 @@ class Klippy:
                 connected = response.is_success
 
                 if connected:
+                    self.hostname = orjson.loads(response.text)["result"].get("hostname", "")
                     return ""
                 # TODO: get reason from error handler
                 last_reason = f"{response.status_code}"
